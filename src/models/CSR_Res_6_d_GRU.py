ed4c6777b3e034c492d65899593c96fcedb07d9b
ccoderz <xzheng.ethz@gmail.com>
2020-07-25 19:04:27 +0800

    update model

--- src/models/CSR_Res_6_d_GRU.py
+++ src/models/CSR_Res_6_d_GRU.py
@@ -21,9 +21,9 @@ def conv1x1(in_features, out_features, stride=1):
     return nn.Conv1d(in_features, out_features, kernel_size=1, stride=stride, bias=True)
 
 
-class CharResCNN(nn.Module):
+class CharResCNN6(nn.Module):
     def __init__(self, args):
-        super(CharResCNN, self).__init__()
+        super(CharResCNN6, self).__init__()
         self.relu = nn.ReLU(inplace=True)
         self.maxpool1d = nn.MaxPool1d(kernel_size=3, stride=3)
         self.bottleneck = nn.Sequential(
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,18 +1,18 @@
 """Model definitions (one class per file) to define models."""
 from .CharCNN import CharCNN
-from .CharResCNN_GRU import CharResCNN_GRU
+from .CSR_Res_6_d_GRU import CharResCNN6
 from .BERT_GRU import BERTGRU
 from .CSR_Res_4_d_GRU import CSRRes4d
 from .CSR_Res_4_GRU import CSRRes4
 
-__all__ = ('CharCNN', 'CharResCNN_GRU', 'BERTGRU', 'CSR_Res_4_d_GRU', 'CSR_Res_4_GRU')
+__all__ = ('CharCNN', 'CSR_Res_6_d_GRU', 'BERTGRU', 'CSR_Res_4_d_GRU', 'CSR_Res_4_GRU')
 
 
 def get_model(name):
     if name == "CharCNN":
         return CharCNN
-    elif name == "CharResCNN_GRU":
-        return CharResCNN_GRU
+    elif name == "CSR_Res_6_d_GRU":
+        return CharResCNN6
     elif name == 'BERT_GRU':
         return BERT_GRU
     elif name == 'CSR_Res_4_d_GRU':
